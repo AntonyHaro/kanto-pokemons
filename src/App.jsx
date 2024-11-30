661b49e50415939e2d17aa7cbf92f927b6e3ec18
AntonyHaro <antonyhpro@outlook.com>
2024-11-29 22:00:04 -0300

    limpeza de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Home from "./pages/Home/Home";
 import PokemonInfo from "./pages/PokemonInfo/PokemonInfo";
 import PokemonMove from "./pages/PokemonMove/PokemonMove";
 import Favorites from "./pages/Favorites/Favorites";
-import Teams from "./pages/Teams/Teams";
 import Comparator from "./pages/Comparator/Comparator";
 import Moves from "./pages/Moves/Moves";
 import Berries from "./pages/Berries/Berries";
